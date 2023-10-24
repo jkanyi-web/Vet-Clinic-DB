1cafa7e6036767f65703be6287ce0daaf4c3c966
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-24 12:50:12 +0300

    Create, Read, Update and Query animals Table data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,17 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL,
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
