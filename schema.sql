d6313b21533a8e0fec6c3d8df6f2927849560b5d
kanyi <jkanyi757@gmail.com>
2023-10-24 10:19:21 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 CREATE TABLE IF NOT EXISTS public.animals
 (
     "id " integer NOT NULL,
