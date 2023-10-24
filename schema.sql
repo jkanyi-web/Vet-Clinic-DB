ef1133e373f0ccc9df0954e9f047da019d7d68f8
kanyi <jkanyi757@gmail.com>
2023-10-24 10:17:33 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    "id " integer NOT NULL,
+    name character(1) COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg real NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY ("id ")
+)
