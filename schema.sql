0c44fbfe1f23ce8789a9ccceb2787ab3b409024d
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-26 19:48:19 +0300

    review changes to replace computer generated SQL code to programmer-generated code

--- schema.sql
+++ schema.sql
@@ -1,115 +1,49 @@
 /* Database schema to keep the structure of entire database. */
 
 -- animals table
-CREATE TABLE IF NOT EXISTS public.animals
-(
-    id integer NOT NULL DEFAULT nextval('new_animals_id_seq'::regclass),
-    full_name character varying(255) COLLATE pg_catalog."default",
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg numeric,
-    species_id integer,
-    owner_id integer,
-    CONSTRAINT new_animals_pkey PRIMARY KEY (id),
-    CONSTRAINT animals_owner_id_fkey FOREIGN KEY (owner_id)
-        REFERENCES public.owners (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT animals_species_id_fkey FOREIGN KEY (species_id)
-        REFERENCES public.species (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.animals
-    OWNER to postgres;
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10, 2),
+    species_id INTEGER REFERENCES species(id),
+    owner_id INTEGER REFERENCES owners(id)
+);
 
 -- owners table
-CREATE TABLE IF NOT EXISTS public.owners
-(
-    id integer NOT NULL DEFAULT nextval('owners_id_seq'::regclass),
-    full_name character varying(255) COLLATE pg_catalog."default",
-    age integer,
-    CONSTRAINT owners_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.owners
-    OWNER to postgres;
-
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR,
+    age INTEGER
+);
 -- species table
-CREATE TABLE IF NOT EXISTS public.species
-(
-    id integer NOT NULL DEFAULT nextval('species_id_seq'::regclass),
-    name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT species_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.species
-    OWNER to postgres;
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
 
 -- DAY 4
 
 -- vets table
-CREATE TABLE IF NOT EXISTS public.vets
-(
-    id integer NOT NULL DEFAULT nextval('vets_id_seq'::regclass),
-    name character varying(100) COLLATE pg_catalog."default",
-    age integer,
-    date_of_graduation date,
-    CONSTRAINT vets_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.vets
-    OWNER to postgres;
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    age INTEGER,
+    date_of_graduation DATE
+);
 
 -- visits table
-CREATE TABLE IF NOT EXISTS public.visits
-(
-    vet_id integer NOT NULL,
-    animal_id integer NOT NULL,
-    visit_date date NOT NULL,
-    CONSTRAINT visits_pkey PRIMARY KEY (vet_id, animal_id, visit_date),
-    CONSTRAINT visits_animal_id_fkey FOREIGN KEY (animal_id)
-        REFERENCES public.animals (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT visits_vet_id_fkey FOREIGN KEY (vet_id)
-        REFERENCES public.vets (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.visits
-    OWNER to postgres;
-
+CREATE TABLE visits (
+    visit_id SERIAL PRIMARY KEY,
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE
+);
 -- specializations table
-CREATE TABLE IF NOT EXISTS public.specializations
-(
-    vet_id integer NOT NULL,
-    species_id integer NOT NULL,
-    CONSTRAINT specializations_pkey PRIMARY KEY (vet_id, species_id),
-    CONSTRAINT specializations_species_id_fkey FOREIGN KEY (species_id)
-        REFERENCES public.species (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT specializations_vet_id_fkey FOREIGN KEY (vet_id)
-        REFERENCES public.vets (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.specializations
-    OWNER to postgres;
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
