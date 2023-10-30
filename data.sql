54cf36519106697f78430a472931c80e9e8f502a
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-10-30 16:28:53 +0300

    create new database, insert values in respective tables and columns and insert huge data to make queries take too much time

--- data.sql
+++ data.sql
@@ -80,3 +80,27 @@ VALUES
 (2, 3, '2020-08-03'),
 (3, 9, '2020-05-24'),
 (1, 9, '2021-01-11');
+
+-- Week 2 DAY 1 vet_clinic_continuation
+
+INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
+
+INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) 
+SELECT * 
+FROM 
+  (SELECT id FROM animals) animal_ids, 
+  (SELECT id FROM vets) vets_ids, 
+  generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+INSERT INTO owners (full_name, email) 
+SELECT 
+  'Owner ' || generate_series(1,2500000), 
+  'owner_' || generate_series(1,2500000) || '@mail.com';
+
